1f18af27bdd668b4e7a6127baf2a641d976ef964
thebestdev <brandonjinukim@gmail.com>
2026-09-12 07:50:20 -0700

    Emergency opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EmergencyTeleOp.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Emergency TeleOp", group = "TeleOp")
+public class EmergencyTeleOp extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double forward = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+            double frontLeftPower = forward + strafe + turn;
+            double backLeftPower = forward - strafe + turn;
+            double frontRightPower = forward - strafe - turn;
+            double backRightPower = forward + strafe - turn;
+            double maxPower = Math.max(1.0, Math.max(Math.abs(frontLeftPower),
+                    Math.max(Math.abs(backLeftPower), Math.max(Math.abs(frontRightPower),
+                            Math.abs(backRightPower)))));
+
+            frontLeft.setPower(frontLeftPower / maxPower);
+            backLeft.setPower(backLeftPower / maxPower);
+            frontRight.setPower(frontRightPower / maxPower);
+            backRight.setPower(backRightPower / maxPower);
+        }
+
+        stopDrive();
+    }
+
+    private void stopDrive() {
+        frontLeft.setPower(0.0);
+        frontRight.setPower(0.0);
+        backLeft.setPower(0.0);
+        backRight.setPower(0.0);
+    }
+}
